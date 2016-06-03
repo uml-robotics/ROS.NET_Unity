446e6cd3d102494a18b665e89e8ef9d8788c8c94
DCurtin <daltoncurtin@gmaill.com>
2016-06-03 14:51:12 -0400

    Added new laser visualizer code. Day is not working, set it to 0 for now.

---
+++ Assets/Scripts/LaserScanView.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using Messages.sensor_msgs;
+using System;
+
+public class LaserScanView : MonoBehaviour
+{
+
+    private float[] distBuffer;
+    private GameObject[] pointBuffer;
+    private bool changed;
+    private DateTime birthday;
+    float angMin, angInc;
+    private float decay
+    {
+        get { return transform.parent == null ? 0f : transform.parent.gameObject.GetComponent<LaserVisController>().decay_time; }
+    }
+    private float pointSize
+    {
+        get { return transform.parent == null ? 1f : transform.parent.gameObject.GetComponent<LaserVisController>().pointSize; }
+    }
+    public delegate void IDiedCallback(GameObject me);
+    public event IDiedCallback IDied;
+
+    internal void expire()
+    {
+        // gameObject.hideFlags |= HideFlags.HideAndDontSave;
+        if (IDied != null)
+            IDied(gameObject);
+    }
+
+    public void SetScan(DateTime msgreceived, LaserScan msg)
+    {
+        //compare length of distbuffer and msg.ranges
+        //recreate distance array
+        angMin = msg.angle_min;
+        angInc = msg.angle_increment;
+        birthday = msgreceived;
+        if (distBuffer == null || distBuffer.Length != msg.ranges.Length)
+            distBuffer = new float[msg.ranges.Length];
+        Array.Copy(msg.ranges, distBuffer, distBuffer.Length);
+        changed = true;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region SHOULD I DIE?
+        if (decay > 0.0001 && DateTime.Now.Subtract(birthday).TotalSeconds > decay)
+        {
+            expire();
+            return;
+        }
+        #endregion
+
+        if (changed)
+        {
+            //show if hidden (this scan was recycled)
+            // hideFlags &= ~HideFlags.HideAndDontSave;
+
+            #region RESIZE IF NEEDED, ADD+REMOVE SPHERES AS NEEDED
+            //resize sphere array if different from distbuffer
+            //remath all circles based on distBuffer
+            if (pointBuffer != null && pointBuffer.Length != distBuffer.Length)
+            {
+                int oldsize = pointBuffer.Length;
+                int newsize = distBuffer.Length;
+                if (oldsize < newsize)
+                {
+                    Array.Resize(ref pointBuffer, newsize);
+                    for (int i = oldsize; i < newsize; i++)
+                    {
+                        GameObject newsphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                        newsphere.transform.SetParent(transform);
+                        pointBuffer[i] = newsphere;
+                    }
+                }
+                else
+                {
+                    for (int i = oldsize; i >= newsize; i--)
+                    {
+                        pointBuffer[i].transform.SetParent(null);
+                        pointBuffer[i] = null;
+                    }
+                    Array.Resize(ref pointBuffer, newsize);
+                }
+            }
+            else if (pointBuffer == null)
+            {
+                pointBuffer = new GameObject[distBuffer.Length];
+                for (int i = 0; i < distBuffer.Length; i++)
+                {
+                    GameObject newsphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    newsphere.transform.SetParent(transform);
+                    pointBuffer[i] = newsphere;
+                }
+            }
+            #endregion
+
+            #region FOR ALL SPHERES ALL THE TIME
+            for (int i = 0; i < pointBuffer.Length; i++)
+            {
+                pointBuffer[i].transform.localScale = new Vector3(pointSize, pointSize, pointSize);
+                //TODO: SET THE POSITION for pointBuffer[i] based on distBuffer[i]
+                pointBuffer[i].transform.localPosition = new Vector3((float)(distBuffer[i] * Math.Sin(angMin + angInc * i)), 1F, (float)(distBuffer[i] * Math.Cos(angMin + angInc * i)));
+            }
+            #endregion
+            changed = false;
+        }
+    }
+}
---
+++ Assets/Scripts/LaserVisController.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using Messages.sensor_msgs;
+using Ros_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class LaserVisController : MonoBehaviour
+{
+    private NodeHandle nh = null;
+    private Subscriber<LaserScan> scansub;
+    SortedList<DateTime, LaserScan> toDraw = new SortedList<DateTime, LaserScan>();
+    List<GameObject> recycle = new List<GameObject>();
+
+    private GameObject points;
+
+    public float pointSize = 1;
+    public string scan_topic;
+    public ROSManager ROSManager;
+    public float decay_time = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        ROSManager.GetComponent<ROSManager>().StartROS(() => {
+            nh = new NodeHandle();
+            scansub = nh.subscribe<LaserScan>(scan_topic, 1, scancb);
+        });
+
+        //get the TEMPLATE view (our only child 
+        points = transform.GetChild(0).gameObject;
+        //points.hideFlags |= HideFlags.HideAndDontSave;
+        points.name = "Points";
+    }
+
+    private void scancb(LaserScan argument)
+    {
+        /*
+            Debug.Log("Penis");
+            Debug.Log(argument.ranges[0].ToString());
+            Debug.Log(argument.ranges[1].ToString());
+            Debug.Log(argument.ranges[2].ToString());
+            Debug.Log("angle_max: " + argument.angle_max.ToString());
+            Debug.Log("angle_min: " + argument.angle_min.ToString());
+            Debug.Log("angle_inc: " + argument.angle_increment.ToString());
+            int angles = (int)((Math.Abs(argument.angle_min) + Math.Abs(argument.angle_max)) / argument.angle_increment);
+            Debug.Log("angles: " + angles.ToString());
+            Debug.Log("Ranges_size: " + argument.ranges.Length.ToString());
+            */
+        lock (toDraw)
+            toDraw.Add(DateTime.Now, argument);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lock (toDraw)
+        {
+            if (decay_time < 0.0001f)
+            {
+                while (toDraw.Count > 1)
+                {
+                    toDraw.RemoveAt(0);
+                }
+
+                if (transform.childCount > 1)
+                {
+                    //transform.GetChild(1).gameObject.hideFlags |= HideFlags.DontSaveInBuild;
+                    lock (recycle)
+                        recycle.Add(transform.GetChild(1).gameObject);
+                }
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    Transform c = transform.GetChild(i);
+                    // eat our siblings
+                    if (i > 1)
+                    {
+                        c.SetParent(null);
+                        Destroy(c.gameObject);
+                    }
+                }
+            }
+            while (toDraw.Count > 0)
+            {
+                GameObject newone = null;
+                bool need_a_new_one = true;
+                lock (recycle)
+                {
+                    if (recycle.Count > 0)
+                    {
+                        need_a_new_one = false;
+                        newone = recycle[0];
+                        recycle.RemoveAt(0);
+                        if (decay_time < 0.0001f)
+                            recycle.Clear();
+                    }
+                }
+                if (need_a_new_one)
+                {
+                    newone = Instantiate(points.transform).gameObject;
+                    newone.transform.SetParent(transform, false);
+                    //newone = ((instantiate a copy of the template))
+                    newone.GetComponent<LaserScanView>().IDied += (deadScan) =>
+                    {
+                        lock (recycle)
+                            recycle.Add(deadScan);
+                    };
+                }
+                KeyValuePair<DateTime, LaserScan> oldest = toDraw.ElementAt(0);
+                toDraw.RemoveAt(0);
+                newone.GetComponent<LaserScanView>().SetScan(oldest.Key, oldest.Value);
+            }
+        }
+    }
+}

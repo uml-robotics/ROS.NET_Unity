00e729ff959eb561dea05c65e3514ccba90cef6d
DCurtin <daltoncurtin@gmaill.com>
2016-06-07 16:03:58 -0400

    Added "mutex interface" a set of add and remove functions for interfacing with the recycle list and toDraw sortedList to make them thread safe while hopefully reducing dead lock
    Working on a null exception thrown at line 140 LaserVisController.cs
    newone.GetComponent<LaserScanView>().SetScan(Time.fixedTime, oldest.Value);
    
    This line is reported in unity as throwing null exceptions, this may be caused by  the way the events IDied and Recycle

--- Assets/Scripts/LaserVisController.cs
+++ Assets/Scripts/LaserVisController.cs
@@ -10,8 +10,8 @@ public class LaserVisController : MonoBehaviour
 {
     private NodeHandle nh = null;
     private Subscriber<LaserScan> scansub;
-    SortedList<uint , LaserScan> toDraw = new SortedList<uint, LaserScan>();
-    List<GameObject> recycle = new List< GameObject>();
+    SortedList<uint, LaserScan> toDraw = new SortedList<uint, LaserScan>();
+    List<GameObject> recycle = new List<GameObject>();
     private GameObject points;
 
     public string scan_topic;
@@ -51,92 +51,161 @@ public class LaserVisController : MonoBehaviour
             Debug.Log("angles: " + angles.ToString());
             Debug.Log("Ranges_size: " + argument.ranges.Length.ToString());
             */
-        lock (toDraw)
-            if(toDraw.Count > 2 && Debug_Messages)
-            {
-                DebugText.WriteLine("First Element: " + toDraw.ElementAt(0).Key.ToString());
-                DebugText.WriteLine("Last Element: " + toDraw.Last().Key.ToString());
-            }
-            toDraw.Add(argument.header.seq , argument);
+
+        if (toDraw.Count > 2 && Debug_Messages)
+        {
+            DebugText.WriteLine("First Element: " + toDraw.ElementAt(0).Key.ToString());
+            DebugText.WriteLine("Last Element: " + toDraw.Last().Key.ToString());
+        }
+        //toDraw.Add(argument.header.seq, argument);
+        addToDraw(argument);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        lock (toDraw)
+
+
+        if (Decay_Time < 0.0001f)
         {
-            if (Decay_Time < 0.0001f)
+           
+            while (toDraw.Count > 1)
             {
-                while (toDraw.Count > 1)
-                {
-                    toDraw.RemoveAt(0);
-                }
-
-                if (transform.childCount > 1)
-                {
-                    //transform.GetChild(1).gameObject.SetActive(false);
-                    //transform.GetChild(1).gameObject.hideFlags |= HideFlags.HideAndDontSave;
-                    lock (recycle)
-                        recycle.Add(transform.GetChild(1).gameObject);
-                }
-                for (int i = 0; i < transform.childCount; i++)
-                {
-                    Transform c = transform.GetChild(i);
-                    // eat our siblings
-                    if (i > 1)
-                    {
-                        c.SetParent(null); //removes child from parents transform list
-                        Destroy(c.gameObject); //removes the game object permantently from the scene
-                    }
-                }
+                remFirstFromToDraw();
             }
-            while (toDraw.Count > 0)
+            
+            if (transform.childCount > 1)
             {
-                GameObject newone = null;
-                bool need_a_new_one = true;
-                lock (recycle)
-                {
-                    if (recycle.Count > 0)
-                    {
-                        need_a_new_one = false;
-                        newone = recycle.ElementAt(0);
-                        recycle.RemoveAt(0);
-                        if (Decay_Time < 0.0001f)
-                            recycle.Clear();
-                    }
-                
-                    if (need_a_new_one)
-                    {
-                        newone = Instantiate(points.transform).gameObject;
-                        newone.transform.SetParent(transform, false);
-                        newone.hideFlags |= HideFlags.HideAndDontSave;
-                        //newone = ((instantiate a copy of the template))
-                        newone.GetComponent<LaserScanView>().Recylce += (oldScan) =>
-                        {
-                            lock (recycle)
-                                recycle.Add(oldScan);
-                        };
+                //transform.GetChild(1).gameObject.SetActive(false);
+                //transform.GetChild(1).gameObject.hideFlags |= HideFlags.HideAndDontSave;
 
-                        newone.GetComponent<LaserScanView>().IDied += (deadScan) =>
-                        {
-                            lock (recycle)
-                            {
-                               
-                                    recycle.Remove(deadScan); //attempt to remove if in recycle
-                                    deadScan.transform.SetParent(null); //disconnect from parent
-                                    Destroy(deadScan); //destroy object
-                            
-                            }
-                        };
-                    }
-                
-                        KeyValuePair<uint, LaserScan> oldest = toDraw.First();
-                        toDraw.Remove(oldest.Key);
-                        newone.GetComponent<LaserScanView>().SetScan(Time.fixedTime, oldest.Value);
-                    
+                //recycle.Add(transform.GetChild(1).gameObject);
+                addToRecycle(transform.GetChild(1).gameObject);
+            }
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Transform c = transform.GetChild(i);
+                // eat our siblings
+                if (i > 1)
+                {
+                    c.SetParent(null); //removes child from parents transform list
+                    Destroy(c.gameObject); //removes the game object permantently from the scene
                 }
             }
         }
+        while (toDraw.Count > 0)
+        {
+            GameObject newone = null;
+            bool need_a_new_one = true;
+
+            int recycle_count;
+
+            lock (recycle)
+            {
+                recycle_count = recycle.Count;
+            }
+
+
+            if (recycle_count > 0)
+            {
+                need_a_new_one = false;
+                newone = remFirstFromRecycle();
+                if (Decay_Time < 0.0001f)
+                    clearRecycle();
+            }
+
+
+            if (need_a_new_one)
+            {
+                newone = Instantiate(points.transform).gameObject;
+                newone.transform.SetParent(transform, false);
+                newone.hideFlags |= HideFlags.HideAndDontSave;
+                //newone = ((instantiate a copy of the template))
+                newone.GetComponent<LaserScanView>().Recylce += (oldScan) =>
+                {
+                    addToRecycle(oldScan);
+                };
+
+                newone.GetComponent<LaserScanView>().IDied += (deadScan) =>
+                {
+                    
+                    remFromRecycle(deadScan);
+                    deadScan.transform.SetParent(null); //disconnect from parent
+                    Destroy(deadScan); //destroy object
+
+                };
+            }
+
+            KeyValuePair<uint, LaserScan> oldest = remFirstFromToDraw();
+            newone.GetComponent<LaserScanView>().SetScan(Time.fixedTime, oldest.Value);
+
+
+        }
+
     }
+
+
+    /**
+        Recycle and ToDraw interface(s) for adding and removing elements safely
+    **/
+
+    #region ToDraw interface
+    void addToDraw(LaserScan scanIn)
+    {
+
+        lock (toDraw)
+        {
+            toDraw.Add(scanIn.header.seq, scanIn);
+        }
+    }
+
+    KeyValuePair<uint, LaserScan> remFirstFromToDraw()
+    {
+        KeyValuePair<uint, LaserScan> scanSeqPairOut;
+        lock (toDraw)
+        {
+            scanSeqPairOut = toDraw.First();
+            toDraw.Remove(scanSeqPairOut.Key);
+        }
+        return scanSeqPairOut;
+    }
+    #endregion
+
+    #region Recycle interface
+    void addToRecycle(GameObject gameObjIn)
+    {
+        lock (recycle)
+        {
+            recycle.Add(gameObjIn);
+        }
+    }
+
+    GameObject remFirstFromRecycle()
+    {
+        GameObject gameObjOut;
+        lock (recycle)
+        {
+            //recycle.Add(gameObjIn);
+            gameObjOut = recycle.First();
+            recycle.RemoveAt(0);
+        }
+        return gameObject;
+    }
+
+    void remFromRecycle(GameObject gameObjOut)
+    {
+        lock (recycle)
+        {
+            recycle.Remove(gameObject);
+        }
+
+    }
+
+    void clearRecycle()
+    {
+        lock(recycle)
+            recycle.Clear();
+
+    }
+    #endregion
 }

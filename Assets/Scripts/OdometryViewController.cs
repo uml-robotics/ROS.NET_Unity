7779b1e31306d84aad3784147f3f0595f11341b5
DCurtin <daltoncurtin@gmail.com>
2016-06-22 21:44:45 -0400

    OdometryViewController Improved
    The angleTolerance seems to match rviz, added a OdometryView prefab with an arrow, OdometryViewController requires a child to treat as the arrow.

--- Assets/Scripts/OdometryViewController.cs
+++ Assets/Scripts/OdometryViewController.cs
@@ -1,26 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
-using System;
 using Ros_CSharp;
 using Messages.nav_msgs;
-using Messages.geometry_msgs;
 
-public class OdometryViewController : SensorTFInterface {
+public class OdometryViewController : ROSMonoBehavior {
 
+    public string topic;
     public double positionTolerance = 0.1d; //Distance from last arrow
     public double angleTolerance = 0.1d; //Angular distance from the last arrow
     public int keep = 100; //number of arrows to keep
     public float arrowLength = 0.4f; //length of arrow
-    public bool debugMsgs = false;
 
     private NodeHandle nh = null;
     private Subscriber<Odometry> subscriber;
 
     private Odometry currentMsg = new Odometry(); //Odometry must be initialized since a null can not be locked
-    private UnityEngine.Vector3 currentPos;
-    private UnityEngine.Vector3 lastPos;
-    private UnityEngine.Quaternion currentQuat;
-    private UnityEngine.Quaternion lastQuat;
+    private Vector3 currentPos;
+    private Vector3 lastPos;
+    private Quaternion currentQuat;
+    private Quaternion lastQuat;
 
     private Queue<GameObject> Arrows = new Queue<GameObject>();
     private GameObject arrowGO; //arrow gameobject used to represent orientation of object
@@ -37,7 +35,6 @@ public class OdometryViewController : SensorTFInterface {
 
     // Use this for initialization
     void Start () {
-        TFName = "agent1/base_link";// TF used for debugging purposses
         rosmanager.StartROS(this, () => {
             nh = new NodeHandle();
             subscriber = nh.subscribe<Odometry>(topic, 1, callBack);
@@ -67,27 +64,15 @@ public class OdometryViewController : SensorTFInterface {
                 return;
             }
 
-            if (debugMsgs)
-            {
-                //UnityEngine.Debug.Log("Scan: " + ((currentMsg.twist == null || currentMsg.pose == null) ? "Empty" : (" x: " + currentMsg.pose.pose.position.x.ToString() + " y: " + currentMsg.pose.pose.position.y.ToString() + " z: " + currentMsg.pose.pose.position.z.ToString())));
-                //UnityEngine.Debug.Log("Scan: " + ((currentMsg.twist == null || currentMsg.pose == null) ? "Empty" : (" delta x: " + posDelta(lastPos, currentPos).x.ToString() + " delta y: " + posDelta(lastPos, currentPos).y.ToString() + " delta z: " + posDelta(lastPos, currentPos).z.ToString())));
-                UnityEngine.Debug.Log("Trans rot: " + (TF == null ? "Nothing" : TF.localRotation.eulerAngles.ToString()));
-                UnityEngine.Debug.Log("before rot: " + ((currentMsg.twist == null || currentMsg.pose == null) ? "Empty" : (" x: " + currentQuat.x.ToString() + " y: " + currentQuat.y.ToString() + " z: " + currentQuat.z.ToString() + " w: " + currentQuat.w.ToString())));
-                UnityEngine.Debug.Log("aftter rot: " + currentQuat.eulerAngles.ToString());
-
-            }
-            
-            //TODO make Angle tolerance better reflect Rviz. 
-            if ( (currentPos - lastPos).magnitude > (positionTolerance + arrowLength) || (Mathf.Abs(((currentQuat.eulerAngles - lastQuat.eulerAngles).y * Mathf.Deg2Rad)) * arrowLength) > angleTolerance )
+            //TODO make Angle tolerance better reflect Rviz. UPDATE Rviz has some weird ass voodoo scalling for their angle tolerance
+            //TODO update all arrows when size changes
+            if ( (currentPos - lastPos).magnitude > (positionTolerance) || (Mathf.Pow(Mathf.DeltaAngle(currentQuat.eulerAngles.y, lastQuat.eulerAngles.y), 2)/14400 > angleTolerance ))
             {
                 GameObject arrow = Instantiate(arrowGO);
-                arrow.transform.position = currentPos;
-                arrow.transform.rotation = (currentQuat * UnityEngine.Quaternion.Euler(90, 0, 0));
+                arrow.transform.rotation = (currentQuat * Quaternion.Euler(90, 0, 0));
+                arrow.transform.position = arrow.transform.TransformVector(new Vector3(0f, arrowLength, 0f)) + currentPos;
                 arrow.SetActive(true);
-                UnityEngine.Transform shaft = arrow.transform.Find("shaft");
-                UnityEngine.Transform head = arrow.transform.Find("head");
-                shaft.localScale = new UnityEngine.Vector3(shaft.localScale.x, arrowLength - 0.1f, shaft.localScale.z); //arrow head is roughly 0.1f long
-                head.localPosition = new UnityEngine.Vector3(head.localPosition.x, arrowLength - 0.1f, head.localPosition.z);
+                arrow.transform.localScale = new Vector3(arrowLength, arrowLength, arrowLength);
                 arrow.hideFlags |= HideFlags.HideInHierarchy;
                 Arrows.Enqueue(arrow);
                 lastPos = currentPos;
@@ -97,14 +82,14 @@ public class OdometryViewController : SensorTFInterface {
         
     }
     
-    UnityEngine.Quaternion RosToUnityQuat(Messages.geometry_msgs.Quaternion Ros_Quat)
+    Quaternion RosToUnityQuat(Messages.geometry_msgs.Quaternion Ros_Quat)
     {
-        return new UnityEngine.Quaternion((float)Ros_Quat.y, -(float)Ros_Quat.z, (float)Ros_Quat.x, (float)Ros_Quat.w); //tempfix: swapping y and x seemed to fix orientation issues
+        return new Quaternion((float)Ros_Quat.y, -(float)Ros_Quat.z, (float)Ros_Quat.x, (float)Ros_Quat.w); //tempfix: swapping y and x seemed to fix orientation issues
     }
 
-    UnityEngine.Vector3 RosPointToVector3(Messages.geometry_msgs.Point ROS_Point)
+    Vector3 RosPointToVector3(Messages.geometry_msgs.Point ROS_Point)
     {
         //return new UnityEngine.Vector3((float)ROS_Point.x, (float)ROS_Point.z, (float)ROS_Point.y);
-        return new UnityEngine.Vector3(-(float)ROS_Point.y, (float)ROS_Point.z, (float)ROS_Point.x); //tempfix: y and x appear to be swapped in message or orientation in scene is off?
+        return new Vector3(-(float)ROS_Point.y, (float)ROS_Point.z, (float)ROS_Point.x); //tempfix: y and x appear to be swapped in message or orientation in scene is off?
     }
 }

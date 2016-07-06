239f7855c673da089f9704da7db17839dd6e2a34
DCurtin <daltoncurtin@gmail.com>
2016-07-06 17:54:02 -0400

    Underp old fashioned unity script generation

--- CopyMeshes.bat
+++ CopyMeshes.bat
@@ -4,11 +4,14 @@ set DEST=%DEST%COPY_TO_UNITY_PROJECT
 goto :findBabies
 goto :eof
 :findBabies
-for /D %%F in (*meshes) do for %%E in (%%~dpF) do for %%G in (%%~dpE) do ( set src=%%G
+for /D %%F in (*meshes) do for %%E in (%%~dpF) do ( 
+set src=%%E
 set MYDIR1=%src:~0,-1%
 for %%f in (%MYDIR1%) do set myfolder=%%~nxf
+if not "%myfolder%" == "-1" (
 echo xcopy /E /I /Y /Q "%MYDIR1%" "%DEST%\%myfolder%"
 xcopy /E /I /Y /Q "%MYDIR1%" "%DEST%\%myfolder%"
+)
 )
 for /D %%A in (*) do ( 
  (Echo "%%A" | FIND /I "COPY_TO_UNITY_PROJECT" 1>NUL) || (

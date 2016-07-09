eda3e71aba49e496bdbde4816754a4895b22a2db
DCurtin <daltoncurtin@gmail.com>
2016-07-08 21:37:24 -0400

    LoadMesh works more better
    Currently works for the mrm_cover, Some hackey lines were added to make it work with Baxter. Whether or not this will work well on all meshes is unknown. Some playing with the rotations will be necessary to find a generic fix for all meshes.
    TODO: 	figure out what's up with Baxters screen.
    	Add material handling for locally defined materials.

--- Assets/Scripts/LoadMesh.cs
+++ Assets/Scripts/LoadMesh.cs
@@ -4,17 +4,16 @@ using Ros_CSharp;
 using System.Collections.Generic;
 using System.Xml.Linq;
 using System;
+using System.IO;
 
 public class LoadMesh : ROSMonoBehavior {
 
-    GameObject root = null;
-    public string nameSpace;
-    public string Package = "";
     public string RobotDescriptionParam = "";
     private string robotdescription;
+    private Dictionary<string, float[]> materials = new Dictionary<string, float[]>();
     public XDocument RobotDescription { get; private set; }
-    private Dictionary<string, joint> joints = new Dictionary<string, joint>();
-    private Dictionary<string, link> links = new Dictionary<string, link>();
+    //private Dictionary<string, joint> joints = new Dictionary<string, joint>();
+   // private Dictionary<string, link> links = new Dictionary<string, link>();
 
     private TfVisualizer tfviz
     {
@@ -26,47 +25,46 @@ public class LoadMesh : ROSMonoBehavior {
 
 
     //ros stuff
+    protected string NameSpace = "";
+    public void setNamespace(string _NameSpace)
+    {
+        NameSpace = _NameSpace;
+    }
+
     internal NodeHandle nh;
     void Start () {
         rosmanager.StartROS(this, () => {
             nh = new NodeHandle();
         });
-        //Object thing0 = Resources.Load(Package + "\\meshes\\" + MeshName);
-        //GameObject thing = Instantiate(thing0) as GameObject;
         Load();
     }
 	
-	// Update is called once per frame
-	void Update () {
-
-        foreach (Transform tf in transform)
-        {
-            Transform tff;
-            if (!tfviz.queryTransforms("/" + nameSpace + "/" + tf.name, out tff))
-            {
-                tff = transform;
-            }
-
-            tf.transform.position = tff.position;
-            tf.transform.rotation = tff.rotation;
-        }
-
-    }
-
     //Written by Eric M.
+    //Modified by Dalton C.
     public bool Load()
     {
-        if (Param.get(RobotDescriptionParam, ref robotdescription))
+        if (!NameSpace.Equals(string.Empty))
+        {
+            if (!RobotDescriptionParam.StartsWith("/"))
+                RobotDescriptionParam = "/" + RobotDescriptionParam;
+        }else
+        {
+            if (RobotDescriptionParam.StartsWith("/"))
+                RobotDescriptionParam.TrimStart('/');
+        }
+
+
+        if (Param.get(NameSpace + RobotDescriptionParam, ref robotdescription))
         {
             return Parse();
         }
         return false;
     }
 
-    //Written by Eric M. (Listener in ROS.NET samples)
-    //Modified by Dalton C.
     private bool Parse(IEnumerable<XElement> elements = null)
     {
+
+        //Written by Eric M. (Listener in ROS.NET samples)
         if (elements == null)
         {
             RobotDescription = XDocument.Parse(this.robotdescription);
@@ -78,9 +76,13 @@ public class LoadMesh : ROSMonoBehavior {
         }
         elements = RobotDescription.Root.Elements();
        
+        //written by Dalton C.
         //grab joints and links
         foreach (XElement element in elements)
         {
+            if (element.Name == "material")
+                handleMaterial(element); 
+
             if (element.Name == "link")
                 handleLink(element);
 
@@ -91,9 +93,10 @@ public class LoadMesh : ROSMonoBehavior {
                     handleLink(link);
             }
 
+            /*
             if(element.Name == "joint")
                  handleJoint(element);
-            
+            */
         }
         /*
            due to how the code hase been changed its likely not necessary to maintaine joint and link obj's 
@@ -133,19 +136,39 @@ public class LoadMesh : ROSMonoBehavior {
         */
         return true;
     }
+    
+    bool handleMaterial(XElement material)
+    {
+        string colorName = material.Attribute("name").Value;
+        XElement color = material.Element("color");
+        string colorVal = color.Attribute("rgba").Value;
+        string[] colorValSplit = colorVal.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+        float[] rbga = new float[colorValSplit.Length];
 
+        for(int index = 0; index < colorValSplit.Length; ++index)
+        {
+            if(!float.TryParse(colorValSplit[index], out rbga[index]))
+            {
+                rbga[index] = 0;
+            }
+        }
+        
+        materials.Add(colorName, rbga);
+        return true;
+    }
+    
     bool handleJoint(XElement joint)
     {
         XElement origin = joint.Element("origin");
         XElement parent = joint.Element("parent");
         XElement child = joint.Element("child");
         string strOrigin = origin == null ? null : origin.Attribute("xyz") == null ? null : origin.Attribute("xyz").Value;
-
+        /*
         if(parent != null && child != null)
         {
             joints.Add(child.Attribute("link").Value, new joint(parent.Attribute("link").Value, child.Attribute("link").Value, strOrigin));
         }
-
+        */
         return true;
     }
 
@@ -158,9 +181,27 @@ public class LoadMesh : ROSMonoBehavior {
         {
             XElement origin = visual.Element("origin");
             XElement geometry = visual.Element("geometry");
+            XElement material = visual.Element("material");
             string xyz = origin == null ? pose == null ? null : pose.Value : origin.Attribute("xyz").Value;
+            string materialName = material == null ? null : material.Attribute("name") == null ? null : material.Attribute("name").Value;
             if ( geometry != null) 
             {
+
+                Color? color = null;
+                if(materialName != null)
+                {
+                    float[] materialVal;
+                    materials.TryGetValue(materialName, out materialVal);
+                    if (materialVal != null) 
+                    {
+                        if (materialVal.Length == 3)
+                            color = new Color(materialVal[0], materialVal[1], materialVal[2]);
+
+                        if (materialVal.Length == 4)
+                            color = new Color(materialVal[0], materialVal[1], materialVal[2], materialVal[3]);
+                    }
+                }
+                
                 //handle mesh
                 XElement mesh;
                 if ((mesh = geometry.Element("mesh")) != null)
@@ -171,16 +212,44 @@ public class LoadMesh : ROSMonoBehavior {
                     {
                         if (path.StartsWith("package://"))
                             path = path.Remove(0, 10);
-                        if (path.EndsWith(".dae"))
-                            path = path.Remove(path.Length - 4, 4);
-                        GameObject go = Instantiate(Resources.Load(path) as GameObject);
-                        go.transform.parent = transform;
-                        foreach(Transform tf in go.transform)
-                        {
-                            tf.rotation *= Quaternion.Euler(0, 0, 90);
+                      //  else
+                       //     throw new Exception("piss");
+                        Debug.Log("Trying to load " + path);
+                        if(path.EndsWith(".dae"))
+                            path = path.Substring(0, path.LastIndexOf("."));
+                        if (path.EndsWith(".DAE"))
+                            path = path.Substring(0, path.LastIndexOf("."));
+
+
+                        //if (!File.Exists(path))
+                        //   Debug.LogError(path + " is missing or some shit");
+                        try {
+                            UnityEngine.Object foundMesh = Resources.Load(path) as GameObject;
+                            if (foundMesh != null)
+                            {
+                                GameObject go = Instantiate(foundMesh as GameObject);
+                                go.transform.parent = transform;
+
+                                if (link.Attribute("name").Value == "right_lower_forearm")
+                                    Debug.Log("thin");
+
+                                foreach (Transform tf in go.transform)
+                                {
+                                    if (tf.localEulerAngles.x == 90)
+                                        tf.rotation *= Quaternion.Euler(180, 0, 0);
+
+                                    tf.localRotation *= Quaternion.Euler(new Vector3(0, 0, 90) + go.transform.localRotation.eulerAngles);
+                                    if (tf.gameObject.GetComponent<MeshRenderer>() != null && color != null)
+                                        tf.gameObject.GetComponent<MeshRenderer>().material.color = color.Value;
+                                }
+                                go.name = link.Attribute("name").Value;
+                            }
                         }
-                        go.name = link.Attribute("name").Value;
-                        links.Add(go.name, new link(go, xyz));
+                        catch(Exception e)
+                        {
+                            Debug.LogWarning(e);
+                        }
+                       // links.Add(go.name, new link(go, xyz));
                     }
                 }
 
@@ -198,7 +267,9 @@ public class LoadMesh : ROSMonoBehavior {
                         go.transform.parent = transform;
                         go.transform.localScale = new Vector3(y, z, x);
                         go.name = link.Attribute("name").Value;
-                        links.Add(go.name, new link(go, xyz));
+                        if (go.GetComponent<MeshRenderer>() != null && color != null)
+                            go.GetComponent<MeshRenderer>().material.color = color.Value;
+                        //links.Add(go.name, new link(go, xyz));
 
                     }
 
@@ -213,9 +284,11 @@ public class LoadMesh : ROSMonoBehavior {
                     {
                         GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
                         go.transform.parent = transform;
-                        go.transform.localScale = new Vector3(fRadius * 2, fLength, fRadius * 2);
+                        go.transform.localScale = new Vector3(fRadius * 2, fLength/2, fRadius * 2);
                         go.name = link.Attribute("name").Value;
-                        links.Add(go.name, new link(go, xyz));
+                        if (go.GetComponent<MeshRenderer>() != null && color != null)
+                            go.GetComponent<MeshRenderer>().material.color = color.Value;
+                        //links.Add(go.name, new link(go, xyz));
                     }
 
                 }
@@ -227,12 +300,35 @@ public class LoadMesh : ROSMonoBehavior {
             GameObject go = new GameObject();
             go.transform.parent = transform;
             go.name = link.Attribute("name").Value;
-            links.Add(go.name, new link(go, null));
+            //links.Add(go.name, new link(go, null));
         }
         return true;
     }
-    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        foreach (Transform tf in transform)
+        {
+            Transform tff;
+            if (tfviz.queryTransforms(NameSpace + "/" + tf.name, out tff))
+            {
+                tf.transform.position = tff.position;
+                tf.transform.rotation = tff.rotation;
+                // tff = transform;
+            }
+
+            //tf.transform.position = tff.position;
+            //tf.transform.rotation = tff.rotation;
+        }
+
+    }
+
+
 }
+/*
 class link
 {
     public GameObject component { set; get; }
@@ -244,10 +340,11 @@ class link
     }
     public float[] localPosToFloat()
     {
-        float[] xyz = new float[3];
+        float[] xyz;
         if (localPos != null)
         {
             string[] poses = localPos.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+            xyz = new float[poses.length];
             for (int index = 0; index < poses.Length; ++index)
             {
                 if (!float.TryParse(poses[index], out xyz[index]))
@@ -278,10 +375,11 @@ class joint
     }
     public float[] localPosToFloat()
     {
-        float[] xyz = new float[3];
+        float[] xyz;
         if (childLocalPos != null)
         {
             string[] poses = childLocalPos.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+            xyz = new float[poses.length];
             for (int index = 0; index < poses.Length; ++index)
             {
                 if (!float.TryParse(poses[index], out xyz[index]))
@@ -297,7 +395,7 @@ class joint
         return xyz;
 
     }
-
+    
 }
-
+*/
 
